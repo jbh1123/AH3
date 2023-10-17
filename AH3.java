73e1973bcf5b77f7378220205d6ba8ec4b9e65d5
Herman <julian.herman@fiserv.com>
2023-10-17 09:29:26 -0400

    implemented recursiveFizzBuzz

--- AH3.java
+++ AH3.java
@@ -1,6 +1,5 @@
 public class AH3
 {
-
     static void fizzBuzz(int n) {
         for (int i=1; i <= n; i++) {
             if (i%3==0 && i%5==0) {
@@ -18,7 +17,28 @@ public class AH3
         }
     }
 
+    static void recursFizzBuzz(int n) {
+        if (n==0) {
+            return;
+        }
+        recursFizzBuzz(n-1);
+        if (n%3==0 && n%5==0) {
+            System.out.print("FizzBuzz ");
+        }
+        else if (n%5==0) {
+            System.out.print("Buzz ");
+        }
+        else if (n%3==0) {
+            System.out.print("Fizz ");
+        }
+        else {
+            System.out.print(n + " ");
+        }
+    }
+
     public static void main(String[] args) {
         fizzBuzz(15);
+        System.out.println();
+        recursFizzBuzz(15);
     }
 }
